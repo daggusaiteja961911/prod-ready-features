2e06000815cf3af89f54f35f077348965429021b
daggusaiteja961911 <daggusaiteja123@gmail.com>
2025-11-19 12:58:01 +0530

    Inital spring boot project commit

---
+++ src/main/java/com/codingshuttle/prod_ready_features/prod_ready_features/entities/PostEntity.java
@@ -0,0 +1,22 @@
+package com.codingshuttle.prod_ready_features.prod_ready_features.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "posts")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class PostEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+}
---
+++ src/main/java/com/codingshuttle/prod_ready_features/prod_ready_features/services/PostServiceImpl.java
@@ -0,0 +1,44 @@
+package com.codingshuttle.prod_ready_features.prod_ready_features.services;
+
+import com.codingshuttle.prod_ready_features.prod_ready_features.dto.PostDTO;
+import com.codingshuttle.prod_ready_features.prod_ready_features.entities.PostEntity;
+import com.codingshuttle.prod_ready_features.prod_ready_features.exceptions.ResourceNotFoundException;
+import com.codingshuttle.prod_ready_features.prod_ready_features.repositories.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class PostServiceImpl implements PostService {
+
+    private final PostRepository postRepository;
+
+    private final ModelMapper modelMapper;
+    @Override
+    public List<PostDTO> getAllPosts() {
+        return postRepository
+                .findAll()
+                .stream()
+                .map(postEntity -> modelMapper.map(postEntity, PostDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public PostDTO createNewPost(PostDTO inputPost) {
+        PostEntity postEntity = modelMapper.map(inputPost, PostEntity.class);
+        PostEntity savedPost = postRepository.save(postEntity);
+        return modelMapper.map(savedPost, PostDTO.class);
+    }
+
+    @Override
+    public PostDTO getPostById(Long postId) {
+        PostEntity postEntity = postRepository
+                .findById(postId)
+                .orElseThrow(() -> new ResourceNotFoundException("Post not found"));
+        return modelMapper.map(postEntity, PostDTO.class);
+    }
+}
